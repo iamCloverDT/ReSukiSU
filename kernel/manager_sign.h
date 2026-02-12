ed0efab7d5e39d542d4165c2ff332e111b3748eb
bachnxuan <bachnxuan@gmail.com>
2026-02-12 14:30:51 +0700

    kernel: add more size/hash pairs, enable multi manager support by default
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- kernel/manager_sign.h
+++ kernel/manager_sign.h
@@ -1,7 +1,7 @@
 #ifndef MANAGER_SIGN_H
 #define MANAGER_SIGN_H
 
-// weishu/KernelSU
+// tiann/KernelSU
 #define EXPECTED_SIZE_WEISHU 0x033b
 #define EXPECTED_HASH_WEISHU                                                   \
     "c371061b19d8c7d7d6133c6a9bafe198fa944e50c1b31c9d8daa8d7f1fc2d2d6"
@@ -16,20 +16,35 @@
 #define EXPECTED_HASH_RSUNTK                                                   \
     "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b"
 
-// ShirkNeko/KernelSU
+// SukiSU-Ultra/SukiSU-Ultra
 #define EXPECTED_SIZE_SHIRKNEKO 0x35c
 #define EXPECTED_HASH_SHIRKNEKO                                                \
     "947ae944f3de4ed4c21a7e4f7953ecf351bfa2b36239da37a34111ad29993eef"
-
-// Neko/KernelSU
-#define EXPECTED_SIZE_NEKO 0x29c
-#define EXPECTED_HASH_NEKO                                                     \
-    "946b0557e450a6430a0ba6b6bccee5bc12953ec8735d55e26139b0ec12303b21"
 
 // ReSukiSU/ReSukiSU
 #define EXPECTED_SIZE_RESUKISU 0x377
 #define EXPECTED_HASH_RESUKISU                                                 \
     "d3469712b6214462764a1d8d3e5cbe1d6819a0b629791b9f4101867821f1df64"
+
+// KOWX712/KernelSU
+#define EXPECTED_SIZE_KOWSU 0x375
+#define EXPECTED_HASH_KOWSU                                                  \
+    "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"
+
+// RapliVx/KernelSU
+#define EXPECTED_SIZE_MAMBO 0x384
+#define EXPECTED_HASH_MAMBO                                                    \
+    "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8"
+
+// KernelSU-Next/KernelSU-Next
+#define EXPECTED_SIZE_NEXT 0x3e6
+#define EXPECTED_HASH_NEXT                                                     \
+    "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7"
+
+// pershoot/KernelSU-Next
+#define EXPECTED_SIZE_PERSHOOT 0x338
+#define EXPECTED_HASH_PERSHOOT                                                 \
+    "f26471a28031130362bce7eebffb9a0b8afc3095f163ce0c75a309f03b644a1f"
 
 // Dynamic Sign
 #define EXPECTED_SIZE_OTHER 0x300
