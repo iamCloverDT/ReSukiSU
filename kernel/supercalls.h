3ec232246b2119d2d6066901b2d761db1049dbb1
bachnxuan <bachnxuan@gmail.com>
2026-02-13 15:41:41 +0700

    kernel: supercalls: fix CHANGE_MANAGER_UID api
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- kernel/supercalls.h
+++ kernel/supercalls.h
@@ -194,6 +194,7 @@ void ksu_supercalls_exit(void);
 
 // extensions
 #define CHANGE_MANAGER_UID 10006
+#define KSU_TOOLKIT_SIGNATURE_INDEX 254
 #define KSU_UMOUNT_GETSIZE 107   // get list size // shit is u8 we cant fit 10k+ on it
 #define KSU_UMOUNT_GETLIST 108   // get list
 #define GET_SULOG_DUMP 10009     // get sulog dump, max, last 100 escalations
